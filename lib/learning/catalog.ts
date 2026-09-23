a90213c6b61516db2faeca618cc28ead64f597c1
Krug2 <andrewkrugbusiness@gmail.com>
2026-09-22 23:58:27 -0500

    taught inequalities

--- lib/learning/catalog.ts
+++ lib/learning/catalog.ts
@@ -1,10 +1,11 @@
 import packData from "@/content/learning-packs/mth-215.json";
 import linearLesson from "@/content/lessons/mth-215/m01-l01.json";
+import inequalityLesson from "@/content/lessons/mth-215/m01-l02.json";
 import { lessonSchema, packSchema } from "./contracts";
 import { availableFamilyIds } from "./generate";
 
 export const learningPacks = [packSchema.parse(packData)];
-export const lessons = [lessonSchema.parse(linearLesson)];
+export const lessons = [lessonSchema.parse(linearLesson),lessonSchema.parse(inequalityLesson)];
 export const learningPack = (courseId: string) => learningPacks.find(pack => pack.courseId === courseId);
 export const lessonById = (courseId: string, lessonId: string) => lessons.find(lesson => lesson.courseId === courseId && lesson.id === lessonId);
 
