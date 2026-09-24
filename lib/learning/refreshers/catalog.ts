6f0b99b906f3ef8012714f93869268262108410a
Krug2 <andrewkrugbusiness@gmail.com>
2026-09-23 21:36:55 -0500

    registered algebra refresher

--- lib/learning/refreshers/catalog.ts
+++ lib/learning/refreshers/catalog.ts
@@ -4,10 +4,13 @@ import l02 from "@/content/lessons/f01/m01-l02.json";
 import l03 from "@/content/lessons/f01/m01-l03.json";
 import l04 from "@/content/lessons/f01/m01-l04.json";
 import path from "@/content/refresher-paths/f01.json";
+import algebraPack from "@/content/learning-packs/f02.json";
+import algebraPath from "@/content/refresher-paths/f02.json";
+import { f02Lessons } from "./f02";
 import { lessonSchema, packSchema } from "../contracts";
 import { refresherPathSchema } from "./contracts";
 
-export const refresherPacks = [packSchema.parse(pack)];
-export const refresherLessons = [l01, l02, l03, l04].map(data => lessonSchema.parse(data));
-export const refresherPaths = [refresherPathSchema.parse(path)];
+export const refresherPacks = [packSchema.parse(pack), packSchema.parse(algebraPack)];
+export const refresherLessons = [...[l01, l02, l03, l04].map(data => lessonSchema.parse(data)), ...f02Lessons];
+export const refresherPaths = [refresherPathSchema.parse(path), refresherPathSchema.parse(algebraPath)];
 export const refresherPath = (courseId: string) => refresherPaths.find(path => path.courseId === courseId);
