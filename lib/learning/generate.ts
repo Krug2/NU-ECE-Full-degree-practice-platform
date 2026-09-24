6a959a4f40bb390548c316350283ad63470ec015
Krug2 <andrewkrugbusiness@gmail.com>
2026-09-24 12:36:19 -0500

    added rules

--- lib/learning/generate.ts
+++ lib/learning/generate.ts
@@ -47,8 +47,10 @@ import { logInverseFamilyIds,logInverseQuestion } from "./families/mth-log-inver
 import { logDomainFamilyIds,logDomainQuestion } from "./families/mth-log-domain";
 import { logGraphFamilyIds,logGraphQuestion } from "./families/mth-log-graph";
 import { logAuditFamilyIds,logAuditQuestion } from "./families/mth-log-audit";
+import { logRuleFamilyIds,logRuleQuestion } from "./families/mth-log-rules";
 
 const generators=new Map<string,typeof linearQuestion>();
+for(const id of logRuleFamilyIds)generators.set(id,logRuleQuestion);
 for(const [ids,generate] of [[logInverseFamilyIds,logInverseQuestion],[logDomainFamilyIds,logDomainQuestion],[logGraphFamilyIds,logGraphQuestion],[logAuditFamilyIds,logAuditQuestion]] as const){
   for(const id of ids){if(generators.has(id))throw new Error("Duplicate family: "+id);generators.set(id,generate);}
 }
