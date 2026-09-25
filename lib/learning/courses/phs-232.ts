1e0c916cc587fc341efac7465d90514dd5220407
Krug2 <andrewkrugbusiness@gmail.com>
2026-09-24 22:13:41 -0500

    listed driving

--- lib/learning/courses/phs-232.ts
+++ lib/learning/courses/phs-232.ts
@@ -2,7 +2,8 @@ import pack from "@/content/learning-packs/phs-232.json";
 import harmonic from "@/content/lessons/phs-232/m01-l01.json";
 import energy from "@/content/lessons/phs-232/m01-l02.json";
 import damping from "@/content/lessons/phs-232/m01-l03.json";
+import driven from "@/content/lessons/phs-232/m01-l04.json";
 import { lessonSchema, packSchema } from "../contracts";
 
 export const phs232Pack = packSchema.parse(pack);
-export const phs232Lessons = [lessonSchema.parse(harmonic), lessonSchema.parse(energy), lessonSchema.parse(damping)];
+export const phs232Lessons = [lessonSchema.parse(harmonic), lessonSchema.parse(energy), lessonSchema.parse(damping), lessonSchema.parse(driven)];
